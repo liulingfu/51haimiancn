007bbb2a97b1f35843bf5e5109c781aa62a91585
liulf <liulf1@qq.com>
2017-08-23 09:49:24 +0800

    add 360 css

--- blockchain_2.php
+++ blockchain_2.php
@@ -26,7 +26,7 @@
             <div style="text-align:center;">
                 <img src="images/左书亮.jpg">
                 <h3 style="font-size:33px;padding-top:0.3em;color: #fe6e6e;">左书亮</h3>
-                <p>讲师</p>
+                <p>技术总监</p>
             </div>
             <p>曾供职于阿里巴巴、美团、乐视等一线互联网公司，专注于搭建区块链应用以及金融领域的创新，曾主导PDFS，资产管理平台等应用区块链技术实现的产品。</p>
         </div>
@@ -42,7 +42,7 @@
             <div style="text-align:center;">
                 <img src="images/blockchain/wuguanghua.jpg">
                 <h3 style="font-size:33px;padding-top:0.3em;color: #fe6e6e;">吴光华</h3>
-                <p>讲师</p>
+                <p>技术总监</p>
             </div>
             <p>分布云科技CTO,专注于区块链智能资产开发，在征信、金融等领域有非常丰富的经验。10年JAVA开发经验，多年区块链开发经验。</p>
         </div>
@@ -76,21 +76,23 @@
         <img src="images/以太网.jpg" height="334" width="538" />
         <img src="images/left.png" width="50" height="50" class="arrow"/>
         <div class="h-course-item-content">
-            <h2>第三部分：以太坊介绍</h2>
-            <p>1、以太坊信息介绍<br/>
-                2、从0开始搭建区块链开发环境<br/>
-                3、如何编写区块链应用<br/>
-                4、以太坊开发框架truffle代码结构解析</p>
+            <h2>第三部分：密码学原理与数据结构</h2>
+            <p>1：Hash函数<br/>
+                2：公钥密码学和离散对数<br/>
+                3：签名方案<br/>
+                4：秘钥分配<br/>
+                5：组播安全和版权保护</p>
         </div>
     </div>
     <div class="h-course-item flex flex-row flex-between">
         <div class="h-course-item-content h-course-item-content-left">
-            <h2>第四部分：Solitidy智能合约编程</h2>
-            <p>1、开发语言Solitidy 介绍<br/>
-                2、搭建开发环境<br/>
-                3、Solitidy 源文件布局<br/>
-                4、Solitidy 智能合约结构<br/>5、Solitidy复杂变量类型<br/>6、Solitidy方法<br/>7、Solitidy继承和事件<br/>
-            8、编程实战，区块链征信系统投票系统、公开拍卖系统</p>
+            <h2>第四部分：以太坊介绍</h2>
+            <p>
+                1:以太坊信息介绍<br/>
+                2:从0开始搭建区块链开发环境<br/>
+                3:如何编写区块链应用<br/>
+                4:以太坊开发框架truffle代码结构解析<br/>
+            </p>
         </div>
         <img src="images/right.png" width="50" height="50" class="arrow"/>
         <img src="images/SOLITIDY智能合约.jpg" height="334" width="538" />
@@ -99,17 +101,29 @@
         <img src="images/超级总.jpg" height="334" width="538" />
         <img src="images/left.png" width="50" height="50" class="arrow"/>
         <div class="h-course-item-content">
-            <h2>第五部分：超级总（Hyperledger）的简介</h2>
-            <p>1、区块链浏览器项目<br/>
-                2、组织孵化器项目<br/>
-                3、锯齿湖孵化器项目<br/></p>
+            <h2>第五部分：Solitidy智能合约编程</h2>
+            <p>
+                1:开发语言solitidy介绍<br/>
+                2:搭建开发环境<br/>
+                3:solitidy源文件布局<br/>
+                4:solitidy 智能合约结构<br/>
+
+                5:solitidy复杂变量类型<br/>
+                6:solitidy方法<br/>
+                7:solitidy继承和事件<br/>
+                8:编程实战，区块链征信系统投票系统，公开拍卖<br/>
+            </p>
         </div>
     </div>
     <div class="h-course-item flex flex-row flex-between">
         <div class="h-course-item-content h-course-item-content-left">
-            <h2>第六部分：BigChinaDB</h2>
-            <p>1、BigchinaDB 的基本介绍与安装<br/>
-                2、BigchinaDB 在实际项目中的应用<br/>
+            <h2>第六部分：超级总账（Hyperledger）的简介</h2>
+            <p>
+                1、docker的介绍与实际使用<br/>
+                2：区块链浏览器项目<br/>
+                3: 组织孵化器项目<br/>
+                4:锯齿湖孵化器项目<br/>
+            </p>
 
         </div>
         <img src="images/right.png" width="50" height="50" class="arrow"/>
@@ -119,31 +133,20 @@
         <img src="images/密码.jpg" height="334" width="538" />
         <img src="images/left.png" width="50" height="50" class="arrow"/>
         <div class="h-course-item-content">
-            <h2>第七部分：密码学原理与数据结构</h2>
-            <p>1、Hash函数<br/>
-                2、公钥密码学和离散数学<br/>
-                3、签名方案<br/>4、秘钥分配<br/>5、组播安全和版权保护<br/></p>
+            <h2>第七部分：BigchainDB</h2>
+            <p>
+                1:bigchaindb的基本介绍与安装过程<br/>
+                2:bigchaindb实际项目中的使用<br/>
+            </p>
         </div>
     </div>
-    <div class="h-course-item flex flex-row flex-between">
-        <div class="h-course-item-content h-course-item-content-left">
-            <h2>第八部分：docker容器技术<br/>
-                </h2>
-            <p>1、docker 技术简介<br/>
-                2、docker 技术原理 <br/>
-                3、docker 的安装部署<br/>
-                4、docker 的使用优化</p>
-        </div>
-        <img src="images/right.png" width="50" height="50" class="arrow"/>
-        <img src="images/docker.jpg" height="334" width="538" />
-    </div>
+
     <div class="h-course-item flex flex-row flex-between">
         <img src="images/实战.jpg" height="334" width="538" />
         <img src="images/left.png" width="50" height="50" class="arrow"/>
         <div class="h-course-item-content">
-            <h2>第九部分：项目实战</h2>
-            <p>1、根据前面说学的知识，老师带领<br/>
-            大家实战金融征信系统项目</p>
+            <h2>第八部分：项目实战</h2>
+            <p>根据前面所学知识，老师带领大家实战金融征信系统项目</p>
         </div>
     </div>
 
