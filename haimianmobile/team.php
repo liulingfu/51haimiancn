2353dc4b36033c27fb7db3eb2c9b6fed15b1aaf0
liulf <liulf1@qq.com>
2018-03-17 21:06:07 +0800

    添加新老师，新课程

--- haimianmobile/team.php
+++ haimianmobile/team.php
@@ -41,13 +41,6 @@
             </div>
         </div>
         <div class="h-team-item flex flex-row flex-start align-center">
-            <img src="images/粟阳@2x.png" />
-            <div class="flex flex-column flex-between">
-                <h2>栗阳</h2>
-                <p>曾任职于搜狐、新浪、百合网，担任商业产品负责人，主要负责商业化产品，对互联网公司的商业模式、变现手法、盈利模式以及商业产品的核心算法有非常丰富的经验；丰富的行业经验，经历了用户数从0-3000万的产品全过程。</p>
-            </div>
-        </div>
-        <div class="h-team-item flex flex-row flex-start align-center">
             <img src="images/张言@2x.png" />
             <div class="flex flex-column flex-between">
                 <h2>张言</h2>
