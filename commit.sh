07d559a87080e5596de6413e1a67769d4138a313
liulf <liulf1@qq.com>
2018-04-13 11:49:20 +0800

    add modify files

--- commit.sh
+++ commit.sh
@@ -1,5 +1,3 @@
-﻿echo "----更新代码----"
-git pull
-echo "---开始上传文件---"
-git add * && git commit -m "add modify files" && git push origin master
+﻿echo "---开始上传文件---"
+git pull && git add * && git commit -m "add modify files" && git push origin master
 echo "---上传完成----"
