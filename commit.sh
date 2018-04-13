d25cb27eafba47d2843fe9b3e712361b1467275f
liulf <liulf1@qq.com>
2018-04-13 11:47:24 +0800

    add modify files

--- commit.sh
+++ commit.sh
@@ -1,4 +1,5 @@
-﻿git pull
+﻿echo "----更新代码----"
+git pull
 echo "---开始上传文件---"
 git add * && git commit -m "add modify files" && git push origin master
-echo "---上传完成"
+echo "---上传完成----"
--- header.php
+++ header.php
@@ -1,11 +1,11 @@
 <meta charset="UTF-8">
 <meta name="360-site-verification" content="277a0a1adde1fd84fc84cc71e41ff5de" />
-<title>海绵学堂</title>
+<title>海绵学堂-最具性价比的产品经理培训机构</title>
 <meta name="author" content="ZhouShuo" />
-<meta name="description" content="fullPage callback function." />
-<meta name="keywords"  content="fullpage,jquery,demo,callbac,function,event" />
+<meta name="description" content="海绵学堂-最具性价比的产品经理培训机构" />
+<meta name="keywords"  content="海绵学堂-最具性价比的产品经理培训机构" />
 <meta name="Resource-type" content="Document" />
-<meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1,user-scalable=no"/>
+<meta name="viewport" content="海绵学堂-最具性价比的产品经理培训机构"/>
 <link rel="stylesheet" href="./css/jquery.fullpage.min.css">
 <link rel="stylesheet" href="./css/base.css">
 <script src="./js/jquery-1.8.3.min.js"></script>
